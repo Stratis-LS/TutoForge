fd6df9c1c6d4e81d98acf14aca55bfdf95004b32
Lucas Scotto <lucas.scotto.france@gmail.com>
2024-10-27 18:14:09 +0100

    Cours 1 - Base

---
+++ src/main/java/com/stratis/tutorial/TutorialMain.java
@@ -0,0 +1,18 @@
+package com.stratis.tutorial;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(TutorialMain.MODID)
+public class TutorialMain
+{
+     public static final String MODID = "tutorial_forge";
+
+     public TutorialMain()
+     {
+          IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+          MinecraftForge.EVENT_BUS.register(this);
+     }
+}
